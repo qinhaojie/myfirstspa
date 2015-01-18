921665d81d9c1d248d61c63ffab62806e6753383
qinhaojie <489649216@qq.com>
2015-01-18 21:01:14 +0800

    init

---
+++ js/spa.js
@@ -0,0 +1,8 @@
+var spa = (function() {
+	var initModule = function($con) {
+		spa.shell.initModule($con);
+	};
+	return {
+		initModule: initModule
+	};
+})();
---
+++ js/spa.shell.js
@@ -0,0 +1,43 @@
+spa.shell = (function() {
+	var
+		configMap = {
+			main_html: "<div class=\"spa-shell-head\">" +
+				"<div class=\"spa-shell-logo\"></div>" +
+				"<div class=\"spa-shell-acct\"></div>" +
+				"<div class=\"spa-shell-search\"></div>" +
+				"</div>" +
+				"<div class=\"spa-shell-main\">" +
+				"<div class=\"spa-shell-main-nav\"></div>" +
+				"<div class=\"spa-shell-main-content\"></div>" +
+				"</div>" +
+				"<div class=\"spa-shell-foot\"></div>" +
+				"<div class=\"spa-shell-chat\"></div>" +
+				"<div class=\"spa-shell-modal\"></div>"
+		},
+		stateMap = {
+			$container: null
+		},
+		jqueryMap = {},
+		setJqueryMap, initModule;
+	//--------------------------dom-----------------------------------------
+	setJqueryMap = function() {
+		var $container = stateMap.$container;
+		jqueryMap = {
+			$container: $container
+		};
+	};
+	//--------------------------end dom-----------------------------------------
+	//--------------------------event-----------------------------------------
+	//--------------------------end event-----------------------------------------
+	//--------------------------public-----------------------------------------
+	initModule = function($con) {
+		stateMap.$container = $con;
+		$con.html(configMap.main_html);
+		setJqueryMap();
+	};
+
+	//--------------------------end public-----------------------------------------
+	return {
+		initModule: initModule
+	};
+})();
