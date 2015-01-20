ef1dc09bd635ab987294f6601dc55559737a13f5
qinhaojie <qinhaojie1992@163.com>
2015-01-20 15:26:38 +0800

    第三章完毕代码

--- js/spa.shell.js
+++ js/spa.shell.js
@@ -18,16 +18,67 @@ spa.shell = (function() {
 			chat_extend_height: 450,
 			chat_retract_height: 15,
 			chat_extend_title: '点击关闭',
-			chat_retract_title: '点击展开'
+			chat_retract_title: '点击展开',
+			anchor_schema_map: {
+				chat: {
+					open: true,
+					closed: true
+				}
+			}
 
 		},
 		stateMap = {
 			$container: null,
-			is_chat_retracted: true
+			is_chat_retracted: true,
+			anchor_map: {}
 		},
 		jqueryMap = {},
-		setJqueryMap, initModule, toggleChat, onclickChat;
+		setJqueryMap, initModule, toggleChat, onclickChat,
+		copyAnchorMap, changeAnchorpart, onHashchange;
+
+	//------------------utility---------------------------------------------	
+	copyAnchorMap = function() {
+		return $.extend(true, {}, stateMap.anchor_map);
+	};
+
+	changeAnchorpart = function(arg_map) {
+		var
+			anchor_map_revise = copyAnchorMap(),
+			bool_return = true,
+			key_name, key_name_dep;
+
+		for (key_name in arg_map) {
+			if (arg_map.hasOwnProperty(key_name)) {
+				if (key_name.indexOf('_') == 0) {
+					continue;
+				}
+
+				anchor_map_revise[key_name] = arg_map[key_name];
+
+				key_name_dep = '_' + key_name;
+				if (arg_map[key_name_dep]) {
+					anchor_map_revise[key_name_dep] = arg_map[key_name_dep];
+				} else {
+					delete anchor_map_revise[key_name_dep];
+					delete anchor_map_revise['_s' + key_name_dep];
+				}
+			}
+		};
+
+		try {
+			$.uriAnchor.setAnchor(anchor_map_revise);
+		} catch (e) {
+			$.uriAnchor.setAnchor(stateMap.anchor_map, null, true);
+			bool_return = false;
+		}
+		return bool_return;
+
+
+	};
+	//------------------end utility-----------------------------------------	
+
 	//--------------------------dom-----------------------------------------
+	//设置dom
 	setJqueryMap = function() {
 		var $container = stateMap.$container;
 		jqueryMap = {
@@ -36,7 +87,7 @@ spa.shell = (function() {
 		};
 	};
 
-
+	//切换chat显示状态  
 	toggleChat = function(dosth, callback) {
 			var
 				px_chat_ht = jqueryMap.$chat.height(),
@@ -57,7 +108,7 @@ spa.shell = (function() {
 					}
 				);
 				return true;
-			}
+			};
 
 			jqueryMap.$chat.animate({
 					height: configMap.chat_retract_height
@@ -76,7 +127,50 @@ spa.shell = (function() {
 		//--------------------------end dom-----------------------------------------
 		//--------------------------event-----------------------------------------
 	onclickChat = function(e) {
-		toggleChat(stateMap.is_chat_retracted);
+
+		/*if (toggleChat(stateMap.is_chat_retracted)) {
+			$.uriAnchor.setAnchor({
+				chat: (stateMap.is_chat_retracted ? 'open' : 'closed')
+			});
+		};*/
+		changeAnchorpart({
+			chat: (stateMap.is_chat_retracted ? 'open' : 'closed')
+		});
+		return false;
+	};
+
+	onHashchange = function() {
+		var
+			anchor_map_previous = copyAnchorMap(),
+			anchor_map_proposed,
+			_s_chat_previous, _s_chat_proposed,
+			s_chat_proposed;
+		try {
+			anchor_map_proposed = $.uriAnchor.makeAnchorMap();
+		} catch (e) {
+			$.uriAnchor.setAnchor(anchor_map_previous, null, true);
+			return false;
+		};
+		stateMap.anchor_map = anchor_map_proposed;
+		_s_chat_previous = anchor_map_previous._s_chat;
+		_s_chat_proposed = anchor_map_proposed._s_chat;
+
+		if (!anchor_map_previous || _s_chat_previous !== _s_chat_proposed) {
+			s_chat_proposed = anchor_map_proposed.chat;
+			switch (s_chat_proposed) {
+				case 'open':
+					toggleChat(true);
+					break;
+				case 'closed':
+					toggleChat(false);
+					break;
+				default:
+					toggleChat(false);
+					delete anchor_map_proposed.chat;
+					$.uriAnchor.setAnchor(anchor_map_proposed, null, true);
+			};
+		}
+
 		return false;
 	};
 	//--------------------------end event-----------------------------------------
@@ -90,6 +184,13 @@ spa.shell = (function() {
 		jqueryMap.$chat
 			.attr('title', configMap.chat_retract_time)
 			.click(onclickChat);
+
+		$.uriAnchor.configModule({
+			schema_map : configMap.anchor_schema_map
+		});
+		$(window)
+			.bind('hashchange',onHashchange)
+			.trigger('hashchange');
 	};
 
 	//--------------------------end public-----------------------------------------
